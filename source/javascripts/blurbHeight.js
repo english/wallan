1eaed3f2ec4cfde6bef2f7932f999ab0bd767c86
Jamie English <jamienglish@gmail.com>
2012-07-15 17:28:14 +0100

    Set equal heights for blurbs

---
+++ source/javascripts/blurbHeight.js
@@ -0,0 +1,11 @@
+var heights = [];
+var els = document.getElementsByClassName("blurb");
+
+for (var i = 0; i < els.length; i++) {
+  heights.push(els[i].offsetHeight);
+}
+
+var minHeight = Math.max.apply(null, heights).toString() + "px";
+for (var i = 0; i < els.length; i++) {
+  els[i].style.minHeight = minHeight;
+}
